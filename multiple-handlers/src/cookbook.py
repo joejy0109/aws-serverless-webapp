d2c7212f2830269441e7c3ddbd1f92c262032819
jeongyong.jo <joejy0109@gmail.com>
2022-12-26 23:55:58 +0900

    dynamodb 연동 샘플코드 완성 - 기타 모듈 코드 정리

--- multiple-handlers/src/cookbook.py
+++ multiple-handlers/src/cookbook.py
@@ -2,27 +2,31 @@ import json
 
 
 def handler(event, context):
-    body = {
-        "message": "This is a CookBook handler!!!",
-        "input": event,
-    }
+    if 'httpMethod' not in event:
+        return {"statusCode": 400, "error": "This is not a http request"}
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    verb = event["httpMethod"]
 
-    return response
+    if verb == "GET":
+        return get()    
+    if verb == "POST":
+        return post()
+    if verb == "PUT":
+        return put()
+    if verb == "DELETE":
+        return delete()
+
+    return {"statusCode": 400, "error": "Invalid request."}
 
 
 def get():
     return {"statusCode": 200, "body": "GET ok."}
 
-
 def post():
     return {"statusCode": 200, "body": "POST ok."}
 
-
 def put():
     return {"statusCode": 200, "body": "PUT ok."}
-
 
 def delete():
     return {"statusCode": 200, "body": "DELETE ok."}
--- multiple-handlers/src/user.py
+++ multiple-handlers/src/user.py
@@ -1,6 +1,7 @@
 import base64
 import datetime
 import json
+from typing import Tuple
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 from flask import Flask, make_response
@@ -14,6 +15,9 @@ table = dynamodb.Table('Users')
 def handler(event, context):
     # return {"statusCode": 200, "body": "121212121"}
 
+    print(event)
+    print(context)
+
     if 'httpMethod' not in event:
         return res(400, {"error": "This is not a http request"})
 
@@ -26,15 +30,24 @@ def handler(event, context):
         return res(200, _get_user(user_id))
 
     if verb == 'POST':
-        # payload = base64.b64decode(event['body']).decode('utf-8')
-        # return res(200, {"body": json.loads(payload)})
-        return res(200, {'response': _post_user()})
+        ok, data = _parse_body(event)
+        if not ok:
+            return res(400, data)
+        response = _put_user(data)
+        return res(200, response)
     
     if verb == 'PUT':
-        return res(200, {'THIS': 'PUT'})
+        user_id = event["pathParameters"]["proxy"]
+        ok, data = _parse_body(event)        
+        if not ok:
+            return res(400, data)
+        response = _update_user(user_id, data)        
+        return res(200, response)
 
     if verb == 'DELETE':
-        return res(200, {'THIS': 'DELETE'})
+        user_id = event["pathParameters"]["proxy"]
+        response = _delete_user(user_id)
+        return res(200, response)
 
     return res(400, {"error": f"Invalid a request HTTP Method: {verb}"})
 
@@ -52,7 +65,7 @@ def _get_user(user_id: str) -> dict:
 
 def _get_users() -> dict:
     # response = table.query(
-    #     KeyConditionExpression=Key('UserId')
+    #     KeyConditionExpression=Key('UserId').eq('')
     # )
     response = table.scan()
     items = response['Items']
@@ -64,29 +77,31 @@ def _get_users() -> dict:
     return items
    
 
-def _post_user() -> dict:
-    response = table.put_item(
-        {
-            'UserId': 'joejy0109@gmail.com',
-            'Name': '조정용',
-            'Age': 43,
-            'Gender': 'male',
-            'Address': '경기도 용인시 기흥구 서그내로 31',
-            'CreateAt': datetime.datetime.now().isoformat()
-        }
-    )
+def _put_user(item) -> dict:
+    # item = {
+    #     'UserId': 'joejy0109@gmail.com',
+    #     'Name': '베트맨',
+    #     'Age': 43,
+    #     'Gender': 'male',
+    #     'Address': '서울시 서초구 강남대로 327',
+    #     'CreateAt': datetime.datetime.now().isoformat()
+    # }
+    item['CreatedAt'] = datetime.datetime.now().isoformat()
+    response = table.put_item(Item=item)
     return response
 
 
-def _put_user(item) -> dict:
-    response = table.update_item(key={
-        'UserId': item['UserId']
+def _update_user(user_id, item) -> dict:
+    response = table.update_item(Key={
+        'UserId': user_id
     },
-    UpdateExpression = 'Set Age = :Age, Gender = :Gender',
+    UpdateExpression = 'Set Age = :Age, Gender = :Gender, Address = :Address',
     ExpressionAttributeValues = {
-        'Age': 43,
-        'Gender': 'male',
-    })
+        ':Age': item['Age'],
+        ':Gender': item['Gender'],
+        ':Address': item['Address'],
+    },
+    ReturnValues='UPDATED_NEW')
     return response
 
 
@@ -95,3 +110,13 @@ def _delete_user(user_id:str) -> dict:
         'UserId': user_id
     })
     return response
+
+
+def _parse_body(event) -> Tuple[bool, dict]:
+    if 'body' not in event:
+        return False, {'error': 'Invalid request.(Not include payload)'}
+
+    json_body = base64.b64decode(event['body'])
+    json_data = json.loads(json_body)
+
+    return True, json_data
